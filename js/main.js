f770ce06b9483ee17a78669d4f303750954a0a16
rabbihosain <56104056+rabbihosain@users.noreply.github.com>
2020-07-28 17:53:23 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var MenuItems = document.getElementById("Menu");
+
+MenuItems.style.maxHeight = "0px";
+
+function menuToggle(){
+    if(MenuItems.style.maxHeight == "0px"){
+        MenuItems.style.maxHeight = "200px";
+    }
+    else{
+        MenuItems.style.maxHeight = "0px";
+    }
+}
+
+// Product Gallery 
+
+var productImg = document.getElementById("productImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function(){
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    productImg.src = smallImg[3].src;
+}
